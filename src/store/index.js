b856a67f1eccfa8e067590ab028a6500c2e7d2fc
Antonio José Masiá <email@antoniomasia.com>
2019-12-19 00:54:07 +0100

    Define initial state to control the tabs component

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,33 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    auction: {
+      buyer: {
+        value: null,
+        active: true,
+        disabled: false,
+      },
+      seller: {
+        value: null,
+        active: false,
+        disabled: true,
+      },
+      success: false,
+    },
+  },
+  mutations: {
+    changeTabState(state, tabs) {
+      tabs.forEach(tab => {
+        state.auction[tab].active
+          ? (state.auction[tab].active = false)
+          : (state.auction[tab].active = true)
+        state.auction[tab].disabled
+          ? (state.auction[tab].disabled = false)
+          : (state.auction[tab].disabled = true)
+      })
+    },
+  },
   actions: {},
   modules: {},
 })
