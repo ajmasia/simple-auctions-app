4ef04ee038d516489256427f085e9ee0cbc18985
Antonio José Masiá <email@antoniomasia.com>
2019-12-19 00:54:52 +0100

    Update components to use the initial state

--- src/components/Auction.vue
+++ src/components/Auction.vue
@@ -2,11 +2,23 @@
   <div>
     <b-card no-body>
       <b-tabs pills card>
-        <b-tab title="Buyer" active>
-          <b-card-text><InLineForm labelText="Offer" /></b-card-text>
+        <b-tab
+          title="Buyer"
+          :disabled="parseDisabledValue('buyer')"
+          :active="parseActiveValue('buyer')"
+        >
+          <b-card-text>
+            <InLineForm labelText="Offer" model="buyer" :tabs="tabs" />
+          </b-card-text>
         </b-tab>
-        <b-tab title="Seller">
-          <b-card-text><InLineForm labelText="Minimun Offer" /></b-card-text>
+        <b-tab
+          title="Seller"
+          :disabled="parseDisabledValue('seller')"
+          :active="parseActiveValue('seller')"
+        >
+          <b-card-text>
+            <InLineForm labelText="Minimun Offer" model="seller" :tabs="tabs" />
+          </b-card-text>
         </b-tab>
       </b-tabs>
     </b-card>
@@ -14,11 +26,28 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import InLineForm from './shared/InLineForm'
+
 export default {
   name: 'Auction',
   components: {
     InLineForm,
   },
+  data: function() {
+    return {
+      tabs: ['buyer', 'seller'],
+    }
+  },
+  computed: {
+    ...mapState(['auction']),
+    parseActiveValue() {
+      return model => (this.auction[model].active ? 'active' : null)
+    },
+    parseDisabledValue() {
+      return model => (this.auction[model].disabled ? 'disabled' : null)
+    },
+  },
+  methods: {},
 }
 </script>
--- src/components/shared/InLineForm.vue
+++ src/components/shared/InLineForm.vue
@@ -2,21 +2,33 @@
   <div>
     <b-form inline>
       <b-input
-        id="inline-form-input-name"
+        :id="model"
         class="mb-2 mr-sm-2 mb-sm-0"
-        v-bind:placeholder="labelText"
+        :placeholder="labelText"
+        v-model="auction[model].value"
       ></b-input>
 
-      <b-button variant="primary">Save</b-button>
+      <b-button variant="primary" v-on:click="changeTabState(tabs)">
+        Save
+      </b-button>
     </b-form>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex'
 export default {
   name: 'InLineForm',
   props: {
     labelText: String,
+    model: String,
+    tabs: Array,
+  },
+  computed: {
+    ...mapState(['auction']),
+  },
+  methods: {
+    ...mapMutations(['changeTabState']),
   },
 }
 </script>
