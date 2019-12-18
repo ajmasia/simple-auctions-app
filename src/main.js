2351fc4bf02ad709cdc48ddfe84535a5fdb9f51f
Antonio José Masiá <email@antoniomasia.com>
2019-12-18 21:08:52 +0100

    Add router file to project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 
@@ -10,6 +11,7 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
 new Vue({
+  router,
   store,
   render: h => h(App),
 }).$mount('#app')
