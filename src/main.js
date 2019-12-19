7de4ff9af590cc2ace08a3a242dc544ac796aa77
Antonio José Masiá <email@antoniomasia.com>
2019-12-20 00:06:54 +0100

    Add vuelidate to the project

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
+import Vuelidate from 'vuelidate'
 
 // Styles
 import './theme/index.scss'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(Vuelidate)
 
 new Vue({
   router,
