605293b6944810a97624a4ac5ba4ab0e7f3828fd
Antonio José Masiá <email@antoniomasia.com>
2019-12-21 10:51:11 +0100

    Add new Modal component

---
+++ src/components/shared/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-modal id="modal-center" centered :title="title" ref="modal">
+    <p class="my-4">{{ content }}</p>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    title: String,
+    content: String,
+  },
+
+  created() {
+    console.log(this.title, this.content)
+  },
+}
+</script>
