4e802f2a589227c7f6ab8c49d3e78f94ef0a9057
Antonio José Masiá <email@antoniomasia.com>
2019-12-23 18:01:37 +0100

    Update config.sample.js file

--- config.example.js
+++ config.example.js
@@ -1,3 +1,13 @@
-export const config = {
+export const appConfig = {
   openWeatherAPIKey: '',
+  openWeatherAPIUrl: 'http://api.openweathermap.org/data/2.5/',
+  coords: {
+    lat: null,
+    long: null,
+  },
+  units: 'metric',
+  tempUnit: 'Cº',
+  lang: 'es',
+  currency: 'EUR',
+  currencyDigits: 2,
 }
