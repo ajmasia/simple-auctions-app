3309c7487daf20f145b2fdd1ce1fa219be592c4d
Antonio José Masiá <email@antoniomasia.com>
2019-12-28 19:13:05 +0100

    Update test with new LangSelector

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -1,27 +1,11 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import Header from '@/components/shared/Header'
-import LangSelector from '@/components/shared/LangSelector'
 import store from '@/store'
 
 describe('Header component', () => {
-  const wrapper = shallowMount(Header, { store })
-  const langSelectorWrapper = shallowMount(LangSelector, { store })
-  const enSelected = langSelectorWrapper.find('b-form-select').element
-
   it('his name is App ', () => {
+    const wrapper = shallowMount(Header, { store })
     expect(wrapper.name()).to.eql('app-header')
-  })
-  it('it renders LangSelector component', () => {
-    expect(wrapper.contains(LangSelector)).is.true
-  })
-  it('when LangSelector data change from es to en, app lang change to en', () => {
-    langSelectorWrapper.setData({ selectedLanguage: 'en' })
-    expect(store.state.curLanguage).to.eql('en')
-  })
-  it('when LangSelector change value es to en, app lang change to en  ', () => {
-    enSelected.value = 'en'
-    enSelected.dispatchEvent(new Event('change'))
-    expect(store.state.curLanguage).to.eql('en')
   })
 })
